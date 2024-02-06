6b957e2eb40b609d6879f01e7514830d7b914cf3
rm_rf <148804ko@gmail.com>
2024-02-06 19:41:15 +0300

    v1.1

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import uvicorn
 from config import *
 from fastapi import FastAPI
 
-dataframe = openpyxl.load_workbook("book.xlsx")
+dataframe = openpyxl.load_workbook("lol.xlsx")
 dataframe1 = dataframe.active
 
 app = FastAPI()
 
+breaks = ["Перемена 10 минут", "Перемена 30 минут", "Перемена 30 минут", "Перемена 10 минут", "Перемена 10 минут",
+          "Конец пар"]
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 def get_groups():
     groups = []
@@ -21,6 +26,86 @@ def get_groups():
             groups.append(cell.value)
     return groups
 
+
+@app.get("/breaks")
+def get_breaks():
+    return {"breaks": breaks}
+
+@app.get("/rings_shedule")
+def get_rings_shedule():
+    return {"rings_shedule": ''}
+
+@app.get("/groups")
+def return_groups():
+    groups = get_groups()
+    return {"groups": groups}
+
+@app.get("/timetableV1/{group}")
+def get_lessons(group: str):
+    classes = {}
+    groups = get_groups()
+    selected_group = None
+    skip_one = False
+
+    day_counter = 0
+    classes_counter = 0
+    try:
+        for row in dataframe1[group_map[group] + "6": group_map[group] + "43"]:
+            for cell in row:
+                if skip_one:
+                    skip_one = False
+                    continue
+
+                if cell.value in groups:
+                    print("Creating group json: " + cell.value)
+                    classes[cell.value] = {}
+                    selected_group = cell.value
+                    skip_one = True
+                else:
+                    if classes_counter == 6:
+                        classes_counter = 0
+                        day_counter += 1
+                        print("============NEW DAY - " + str(day_counter) + "============")
+
+                    if classes_counter == 0:
+                        print("value: " + str(cell.value))
+                        print("creating classes arry: " + selected_group + " day_counter " + str(day_counter))
+                        classes[selected_group][day_counter] = []
+
+                        class_to_write = str(cell.value).replace("None", "Нет урока")
+
+                        replace_class = dataframe1.cell(row=cell.row, column=cell.column + 1).value
+                        class_room_number = str(dataframe1.cell(row=cell.row, column=cell.column + 2).value).replace(".0",
+                                                                                                                     "")
+
+                        if replace_class is not None:
+                            class_to_write += " | " + str(replace_class)
+
+                        classes[selected_group][day_counter].append(class_to_write + " ~ " + class_room_number)
+                    else:
+                        print("adding class: " + str(cell.value).replace("None", "Нет урока"))
+                        class_to_write = str(cell.value).replace("None", "Нет урока")
+
+                        replace_class = dataframe1.cell(row=cell.row, column=cell.column + 1).value
+                        class_room_number = str(dataframe1.cell(row=cell.row, column=cell.column + 2).value).replace(".0",
+                                                                                                                     "")
+
+                        if replace_class is not None:
+                            print(replace_class)
+                            class_to_write += " | " + str(replace_class)
+
+                        classes[selected_group][day_counter].append(class_to_write + " ~ " + class_room_number)
+
+                    classes_counter += 1
+    except:
+        classes[group] = {"0": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"], "1": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"2": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"3": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"4": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"5": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"]}
+
+    #объединяем перемены с уроками
+
+    for lesson in classes[group]:
+        classes[group][lesson] = [val for pair in zip(classes[group][lesson], breaks) for val in pair]
+
+    return classes[group]
 @app.get("/timetable/{group}")
 def get_group_lessons(group: str):
     classes = {}
@@ -30,56 +115,59 @@ def get_group_lessons(group: str):
 
     day_counter = 0
     classes_counter = 0
+    try:
+        for row in dataframe1[group_map[group] + "6": group_map[group] + "43"]:
+            for cell in row:
+                if skip_one:
+                    skip_one = False
+                    continue
 
-    for row in dataframe1["E6":"E43"]:
-        for cell in row:
-            if skip_one:
-                skip_one = False
-                continue
-
-            if cell.value in groups:
-                print("Creating group json: " + cell.value)
-                classes[cell.value] = {}
-                selected_group = cell.value
-                skip_one = True
-            else:
-                if classes_counter == 6:
-                    classes_counter = 0
-                    day_counter += 1
-                    print("============NEW DAY - " + str(day_counter) + "============")
-
-                if classes_counter == 0:
-                    print("value: " + str(cell.value))
-                    print("creating classes arry: " + selected_group + " day_counter " + str(day_counter))
-                    classes[selected_group][day_counter] = []
-
-                    class_to_write = str(cell.value).replace("None", "Нет урока")
-
-                    replace_class = dataframe1.cell(row=cell.row, column=cell.column + 1).value
-                    class_room_number = str(dataframe1.cell(row=cell.row, column=cell.column + 2).value).replace(".0",
-                                                                                                                 "")
-
-                    if replace_class is not None:
-                        class_to_write += " | " + str(replace_class)
-
-                    classes[selected_group][day_counter].append(class_to_write + " ~ " + class_room_number)
+                if cell.value in groups:
+                    print("Creating group json: " + cell.value)
+                    classes[cell.value] = {}
+                    selected_group = cell.value
+                    skip_one = True
                 else:
-                    print("adding class: " + str(cell.value).replace("None", "Нет урока"))
-                    class_to_write = str(cell.value).replace("None", "Нет урока")
+                    if classes_counter == 6:
+                        classes_counter = 0
+                        day_counter += 1
+                        print("============NEW DAY - " + str(day_counter) + "============")
 
-                    replace_class = dataframe1.cell(row=cell.row, column=cell.column + 1).value
-                    class_room_number = str(dataframe1.cell(row=cell.row, column=cell.column + 2).value).replace(".0",
-                                                                                                                 "")
+                    if classes_counter == 0:
+                        print("value: " + str(cell.value))
+                        print("creating classes arry: " + selected_group + " day_counter " + str(day_counter))
+                        classes[selected_group][day_counter] = []
 
-                    if replace_class is not None:
-                        print(replace_class)
-                        class_to_write += " | " + str(replace_class)
+                        class_to_write = str(cell.value).replace("None", "Нет урока")
 
-                    classes[selected_group][day_counter].append(class_to_write + " ~ " + class_room_number)
+                        replace_class = dataframe1.cell(row=cell.row, column=cell.column + 1).value
+                        class_room_number = str(dataframe1.cell(row=cell.row, column=cell.column + 2).value).replace(".0",
+                                                                                                                     "")
 
-                classes_counter += 1
+                        if replace_class is not None:
+                            class_to_write += " | " + str(replace_class)
+
+                        classes[selected_group][day_counter].append(class_to_write + " ~ " + class_room_number)
+                    else:
+                        print("adding class: " + str(cell.value).replace("None", "Нет урока"))
+                        class_to_write = str(cell.value).replace("None", "Нет урока")
+
+                        replace_class = dataframe1.cell(row=cell.row, column=cell.column + 1).value
+                        class_room_number = str(dataframe1.cell(row=cell.row, column=cell.column + 2).value).replace(".0",
+                                                                                                                     "")
+
+                        if replace_class is not None:
+                            print(replace_class)
+                            class_to_write += " | " + str(replace_class)
+
+                        classes[selected_group][day_counter].append(class_to_write + " ~ " + class_room_number)
+
+                    classes_counter += 1
+    except:
+        classes[group] = {"0": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"1": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"2": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"3": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"4": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"],"5": ["Server Error", "Server Error", "Server Error","Server Error", "Server Error", "Server Error"]}
 
     return classes
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ test.py
@@ -0,0 +1,10 @@
+import openpyxl
+from config import *
+
+dataframe = openpyxl.load_workbook("test1.xlsx")
+dataframe1 = dataframe.active
+for group_let in group_map:
+    if group_let in dataframe1[group_map[group_let] + "" + str(y_offset)].value:
+        pass
+    else:
+        print(group_let + " - bad")
